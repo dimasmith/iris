fb5186c6560a4d1697bbefffca5c6664effdeb52
Dmytro Kovalchuk <dimasmith@gmail.com>
2019-11-09 16:08:29 +0200

    feat: configure bank and accounting system via properties
    `iris.banking` stands for the bank.
    `iris.accounting` for the accounting system.
    Only `demo` systems are supported now

--- src/main/java/net/anatolich/iris/infra/demo/DemoInfraConfiguration.java
+++ src/main/java/net/anatolich/iris/infra/demo/DemoInfraConfiguration.java
@@ -1,21 +1,28 @@
 package net.anatolich.iris.infra.demo;
 
+import lombok.extern.java.Log;
 import net.anatolich.iris.domain.settlement.AccountingSystem;
 import net.anatolich.iris.domain.settlement.Bank;
 import net.anatolich.iris.domain.settlement.SettlementSettingsRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Log
 @Configuration
 public class DemoInfraConfiguration {
 
     @Bean
+    @ConditionalOnProperty(name = "iris.banking", havingValue = "demo", matchIfMissing = true)
     public Bank bank() {
+        log.info("using banking: demo");
         return new DemoBank();
     }
 
     @Bean
+    @ConditionalOnProperty(name = "iris.accounting", havingValue = "demo", matchIfMissing = true)
     public AccountingSystem accountingSystem() {
+        log.info("using accounting: demo");
         return new DemoAccountingSystem();
     }
 
