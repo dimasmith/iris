4efe7d9c2997d46230aa9f6ec45eb48f61121ecb
Dmytro Kovalchuk <dimasmith@gmail.com>
2019-11-10 23:41:25 +0200

    fix: cover demo scenario with feature test

---
+++ src/test/java/net/anatolich/iris/demo/SettlementDemoTest.java
@@ -0,0 +1,35 @@
+package net.anatolich.iris.demo;
+
+import net.anatolich.iris.domain.settlement.AccountingAccount;
+import net.anatolich.iris.domain.settlement.BalanceComparison;
+import net.anatolich.iris.domain.settlement.BankAccount;
+import net.anatolich.iris.domain.settlement.SettlementService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementDemoTest {
+
+    @Autowired
+    private SettlementService settlements;
+
+    @Test
+    void configureAccountsAndCheckSettlement() {
+        final AccountingAccount accountingAccount = settlements.listAvailableAccountingAccounts().get(0);
+        settlements.selectAccountingAccount(accountingAccount.getId());
+
+        final BankAccount bankAccount = settlements.listAvailableBankAccounts().get(0);
+        settlements.selectBankAccount(bankAccount.getId());
+
+        final BalanceComparison comparison = settlements.compareAccountingAndBankBalances();
+
+        Assertions.assertThat(comparison.getAccountingBalance())
+                .isEqualTo(accountingAccount.getBalance());
+
+        Assertions.assertThat(comparison.getBankingBalance())
+                .isEqualTo(bankAccount.getBalance());
+    }
+}
+
