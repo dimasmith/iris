76f6d317972cd950e0e3b86d3843c95131135790
Dmytro Kovalchuk <dimasmith@gmail.com>
2019-11-12 08:57:05 +0200

    fix: show total balance of settlement

--- src/main/java/net/anatolich/iris/domain/settlement/BalanceComparison.java
+++ src/main/java/net/anatolich/iris/domain/settlement/BalanceComparison.java
@@ -11,10 +11,16 @@ public class BalanceComparison {
     private final boolean settled;
     private final MonetaryAmount bankingBalance;
     private final MonetaryAmount accountingBalance;
+    private final Money balance;
 
     BalanceComparison(Money bankingBalance, Money accountingBalance) {
         this.bankingBalance = Objects.requireNonNull(bankingBalance);
         this.accountingBalance = Objects.requireNonNull(accountingBalance);
         this.settled = bankingBalance.isEqualTo(accountingBalance);
+        this.balance = calculateBalance(bankingBalance, accountingBalance);
+    }
+
+    private Money calculateBalance(Money bankingBalance, Money accountingBalance) {
+        return bankingBalance.subtract(accountingBalance).abs();
     }
 }
--- src/test/java/net/anatolich/iris/domain/settlement/BalanceComparisonTest.java
+++ src/test/java/net/anatolich/iris/domain/settlement/BalanceComparisonTest.java
@@ -4,8 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BalanceComparisonTest {
 
     @Test
@@ -21,6 +19,18 @@ class BalanceComparisonTest {
     }
 
     @Test
+    void balanceOfSettledAccountsIsZero() {
+        Money bankingBalance = Money.of(1000, "UAH");
+        Money accountingBalance = Money.of(1000, "UAH");
+
+        BalanceComparison comparison = new BalanceComparison(bankingBalance, accountingBalance);
+
+        Assertions.assertThat(comparison.getBalance())
+                .as("balance of settled accounts must be 0")
+                .isEqualTo(Money.of(0, "UAH"));
+    }
+
+    @Test
     void balanceNotSettledOnDifferentAmounts() {
         Money bankingBalance = Money.of(902, "UAH");
         Money accountingBalance = Money.of(860, "UAH");
@@ -30,5 +40,30 @@ class BalanceComparisonTest {
         Assertions.assertThat(comparison.isSettled())
                 .as("comparison must be not settled for different amounts")
                 .isFalse();
+    }
+
+    @Test
+    void balanceOfUnsettledAccountsMustEqualAmountDifference() {
+        Money bankingBalance = Money.of(902, "UAH");
+        Money accountingBalance = Money.of(860, "UAH");
+
+        BalanceComparison comparison = new BalanceComparison(bankingBalance, accountingBalance);
+
+        Assertions.assertThat(comparison.getBalance())
+                .as("settlement balance of accounts must be non-zero")
+                .isEqualTo(Money.of(42, "UAH"));
+
+    }
+    @Test
+    void balanceOfUnsettledAccountsMustBeNonNegative() {
+        Money bankingBalance = Money.of(130, "UAH");
+        Money accountingBalance = Money.of(330, "UAH");
+
+        BalanceComparison comparison = new BalanceComparison(bankingBalance, accountingBalance);
+
+        Assertions.assertThat(comparison.getBalance())
+                .as("settlement balance of accounts must be non-zero")
+                .isEqualTo(Money.of(200, "UAH"));
+
     }
 }
