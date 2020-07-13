09c161221c01e4d14be0d87d3fc754b832459e8a
Dmytro Kovalchuk <dimasmith@gmail.com>
2020-07-14 00:28:23 +0300

    fix: initialize api clients in test

--- src/test/java/net/anatolich/iris/subscription/ManageSubscriptionsTest.java
+++ src/test/java/net/anatolich/iris/subscription/ManageSubscriptionsTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import net.anatolich.iris.subscription.infra.rest.SubscriptionDto;
 import net.anatolich.iris.subscription.infra.rest.SubscriptionDto.ServiceDto;
@@ -15,6 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -49,5 +53,14 @@ class ManageSubscriptionsTest {
             .andExpect(jsonPath("$[0].rate.currency", equalTo(rate.getCurrency().getCurrencyCode())))
             .andExpect(jsonPath("$[0].rate.amount", closeTo(
                 rate.getNumberStripped().doubleValue(), 0.001)));
+    }
+
+    @TestConfiguration
+    static class RestClientConfiguration {
+
+        @Bean
+        public SubscriptionRestClient subscriptionRestClient(MockMvc mockMvc, ObjectMapper json) {
+            return new SubscriptionRestClient(mockMvc, json);
+        }
     }
 }
