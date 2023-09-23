f73e6df4a2b0109411baeb3177c0eb5cc2eafab1
Dmytro Kovalchuk <dimasmith@gmail.com>
2023-09-23 22:10:55 +0300

    feat: use Postgres database by default

--- src/test/java/net/anatolich/iris/ContainersConfiguration.java
+++ src/test/java/net/anatolich/iris/ContainersConfiguration.java
@@ -3,14 +3,18 @@ package net.anatolich.iris;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainersConfiguration {
 
     @Bean
     @ServiceConnection
-    public MariaDBContainer<?> mariaDBContainer() {
-        return new MariaDBContainer<>("mariadb:10.8-focal");
+    public PostgreSQLContainer<?> mariaDBContainer() {
+        return new PostgreSQLContainer<>("postgres:15-alpine")
+                .withUsername("iris")
+                .withPassword("s3cr3t")
+                .withDatabaseName("iris")
+                .withReuse(true);
     }
 }
