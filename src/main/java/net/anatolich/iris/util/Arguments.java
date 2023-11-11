71b9866deb3b8a3fb4026a45ab824a5b666c0eee
Dmytro Kovalchuk <dimasmith@gmail.com>
2023-11-11 11:34:08 +0200

    fix(deps): support predicates in arguments checks

--- src/main/java/net/anatolich/iris/util/Arguments.java
+++ src/main/java/net/anatolich/iris/util/Arguments.java
@@ -1,20 +1,33 @@
 package net.anatolich.iris.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Arguments {
 
-    public static void checkArgument(boolean correct, String message) {
+    public static <T> T assertArgument(T argument, boolean correct, String message) {
         if (!correct) {
             throw new IllegalArgumentException(message);
         }
+        return argument;
+    }
+
+    public static <T> T assertArgument(T argument, Predicate<? super T> correct, String message) {
+        if (!correct.test(argument)) {
+            throw new IllegalArgumentException(message);
+        }
+        return argument;
     }
 
     public static <T> T rejectNull(T argument, String message) {
-        checkArgument(argument != null, message);
-        return argument;
+        return assertArgument(argument, Objects::nonNull, message);
     }
 
     public static String rejectEmptyString(String argument, String message) {
-        checkArgument(argument != null && !argument.isBlank(), message);
-        return argument;
+        return assertArgument(argument, argument != null && !argument.isBlank(), message);
     }
 }
